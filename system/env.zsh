52fc6f7cba7fa61e23b405e896f49813d85380b9
Sean Doyle <sean.p.doyle24@gmail.com>
2013-04-11 21:23:22 -0400

    moved env stuff to zshenv.symlink

--- system/env.zsh
+++ system/env.zsh
@@ -2,6 +2,6 @@ export EDITOR='subl'
 
 # boxen setup if it exists
 [ -f /opt/boxen/env.sh ] && source /opt/boxen/env.sh
-[ -f $BOXEN_NVM_DIR/nvm.sh ] && 
-  source $BOXEN_NVM_DIR/nvm.sh  && 
-  nvm use v0.8 > /dev/null 2>&1
+# [ -f $BOXEN_NVM_DIR/nvm.sh ] && 
+#   source $BOXEN_NVM_DIR/nvm.sh  && 
+#   nvm use v0.8 > /dev/null 2>&1
